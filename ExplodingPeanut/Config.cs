59047fa99aac4e44cd1c19d1da5b9fc9b1f98746
gamendegamer321 <gamendegamer321@gmail.com>
2025-09-22 21:22:34 +0200

    Implementation

---
+++ ExplodingPeanut/Config.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace ExplodingPeanut;
+
+public class Config
+{
+    [Description("The chance SCP-173 will explode when it dies (0 = never, 100 = always)")]
+    public float ExplodeChance { get; set; } = 100;
+
+    [Description("The fuse time the grenade on SCP-173 will be spawned with")]
+    public float FuseTime { get; set; } = 0f;
+}
---
+++ ExplodingPeanut/Plugin.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using InventorySystem;
+using InventorySystem.Items.ThrowableProjectiles;
+using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Events.Handlers;
+using LabApi.Features;
+using LabApi.Features.Wrappers;
+using LabApi.Loader.Features.Plugins;
+using Mirror;
+using PlayerRoles;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = System.Random;
+using ThrowableItem = InventorySystem.Items.ThrowableProjectiles.ThrowableItem;
+using TimedGrenadePickup = InventorySystem.Items.ThrowableProjectiles.TimedGrenadePickup;
+
+namespace ExplodingPeanut;
+
+public class Plugin : Plugin<Config>
+{
+    public override string Name => "ExplodingPeanut";
+    public override string Description => "Peanut will explode when killed ";
+    public override string Author => "gamendegamer321";
+    public override Version Version => new(1, 0, 0);
+    public override Version RequiredApiVersion => new(LabApiProperties.CompiledVersion);
+
+    private static readonly Random Random = new();
+
+    private static ThrowableItem Template
+    {
+        get
+        {
+            if (_template == null)
+            {
+                InventoryItemLoader.TryGetItem(ItemType.GrenadeHE, out _template);
+            }
+
+            return _template;
+        }
+    }
+
+    private static ThrowableItem _template;
+
+    public override void Enable()
+    {
+        PlayerEvents.Death += OnPlayerDied;
+    }
+
+    public override void Disable()
+    {
+        PlayerEvents.Death -= OnPlayerDied;
+    }
+
+    private void OnPlayerDied(PlayerDeathEventArgs ev)
+    {
+        if (ev.OldRole != RoleTypeId.Scp173 || Random.Next(100) >= Config.ExplodeChance || Template == null) return;
+
+        var grenade = Object.Instantiate(Template.Projectile, ev.OldPosition, Quaternion.identity);
+        if (grenade is TimeGrenade time)
+        {
+            time._fuseTime = Config.FuseTime;
+        }
+
+        NetworkServer.Spawn(grenade.gameObject);
+        grenade.ServerActivate();
+    }
+}
